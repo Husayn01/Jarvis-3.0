3ad8b119160e19e0e201acb0d8430761b347438f
Hussayn <94265187+Husayn01@users.noreply.github.com>
2022-12-27 15:08:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+
 import './index.css';
 import Firstrow from './components/Firstrow';
 import Secondrow from './components/Secondrow';
